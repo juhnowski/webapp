3ccc658a84429f17134c9ce749cf0f458ff313eb
Juhnowski <juhnowski@gmail.com>
2016-11-11 19:12:01 +0300

    add redux to dependancy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
-import './index.css';
 
 let store = createStore(todoApp)
 
